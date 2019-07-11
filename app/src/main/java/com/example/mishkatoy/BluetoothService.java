988b4596fa417217f2b688fc652a8fdde02f0f63
igrztv <zotov.bmstu@gmail.com>
2019-07-11 09:15:02 +0200

    Can search, connect and disconnect

--- app/src/main/java/com/example/mishkatoy/BluetoothService.java
+++ app/src/main/java/com/example/mishkatoy/BluetoothService.java
@@ -1,112 +1,177 @@
 package com.example.mishkatoy;
 
+import android.app.ListActivity;
 import android.app.Service;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothSocket;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.Handler;
-import android.os.HandlerThread;
 import android.os.IBinder;
-import android.os.Looper;
-import android.os.Message;
-import android.os.Process;
-import android.os.SystemClock;
 import android.util.Log;
-import android.widget.Toast;
 
-import static android.support.v4.app.ActivityCompat.startActivityForResult;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
+import static android.support.constraint.Constraints.TAG;
 
 public class BluetoothService extends Service {
 
-    BluetoothAdapter bluetoothAdapter;
-    public static final String BLE_RESPONSE = "ble_response";
-    public static final String BLE_REQUEST = "ble_request";
+    private int counter = 0;
+    private boolean counterRunning = false;
 
-    private final IBinder mBinder = new BluetoothBinder();
+    private final IBinder binder = new LocalBinder();
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothSocket mmSocket;
 
-    private int counter = 1;
-    private boolean runBluetoothThread = false;
+    private Set<BluetoothDevice> pairedDevices;
 
-    public int getCounter() {
-        return counter;
+    public int startBLE() {
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
+
+        if (bluetoothAdapter == null) {
+            return -1;
+        }
+
+        if (!bluetoothAdapter.isEnabled()) {
+            if (bluetoothAdapter.enable()) {
+                return 0;
+            } else {
+                return -1;
+            }
+        }
+
+        return 0;
     }
 
-    public class BluetoothBinder extends Binder {
+    public void stopBLE() {
+
+    }
+
+    public void searchBLE() {
+        /*
+        BluetoothLeScanner leScanner = bluetoothAdapter.getBluetoothLeScanner();
+        leScanner.startScan();
+
+        scanCallback */
+
+        if (bluetoothAdapter.startDiscovery()) {
+            // activate spinner for best UI expierence
+        }
+    }
+
+    public Set<BluetoothDevice> getDevices() {
+        pairedDevices = bluetoothAdapter.getBondedDevices();
+        Log.e("BLE", "paired device count: " + pairedDevices.size());
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                Log.i("BLE", deviceName + " = " + deviceHardwareAddress);
+            }
+        }
+        return pairedDevices;
+    }
+
+    public boolean connectBLE(BluetoothDevice device) {
+
+        if (mmSocket != null && mmSocket.isConnected()) {
+            disconnectBLE();
+        }
+
+        final BluetoothDevice mDevice = device;
+
+        if (!counterRunning) {
+            counterRunning = true;
+            new Thread(new Runnable() {
+                public void run() {
+                    BluetoothSocket tmp = null;
+                    try {
+                        // Get a BluetoothSocket to connect with the given BluetoothDevice.
+                        // MY_UUID is the app's UUID string, also used in the server code.
+                        UUID MY_UUID = new UUID(0x11112222,0x33334444);
+                        tmp = mDevice.createRfcommSocketToServiceRecord(MY_UUID);
+                    } catch (IOException e) {
+                        counterRunning = false;
+                        Log.e("BleutoothSocket", "Socket's create() method failed", e);
+                    }
+                    mmSocket = tmp;
+
+                    // Cancel discovery to help the connection
+                    bluetoothAdapter.cancelDiscovery();
+
+                    try {
+                        // Connect to the remote device through the socket. This call blocks
+                        // until it succeeds or throws an exception.
+                        mmSocket.connect();
+                    } catch (IOException connectException) {
+                        // Unable to connect; close the socket and return.
+                        Log.e("BleutoothSocket", "Could not connect. Close the client socket", connectException);
+                        counterRunning = false;
+                        try {
+                            mmSocket.close();
+                        } catch (IOException closeException) {
+                            Log.e("BleutoothSocket", "Could not close the client socket", closeException);
+                        }
+                    }
+                }
+            }).start();
+        }
+
+        return true; // == connecting...
+    }
+
+    public void disconnectBLE() {
+        try {
+            Log.e("BleutoothSocket", "Close the client socket by request");
+            mmSocket.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Could not close the client socket", e);
+        }
+    }
+
+    public boolean sendCommand(String cmd) {
+        if (mmSocket.isConnected()) {
+
+        }
+        return false;
+    }
+
+    public String receiveResponse() {
+        String response = "";
+        return response;
+    }
+
+    public class LocalBinder extends Binder {
         BluetoothService getService() {
             return BluetoothService.this;
         }
     }
 
-    public void vibrate() {
-        Log.e("BLE Service", "vibrating");
-    }
-
-    private void Tick() {
-        if (!runBluetoothThread) {
-            runBluetoothThread = true;
-            new Thread(new Runnable() {
-                public void run() {
-                    try {
-                        while(true) {
-                            counter += 1;
-                            Log.e("BLE Service", "running" + Integer.toString(counter));
-                            Thread.sleep(1000);
-                        }
-                    } catch (InterruptedException e) {
-                        // Restore interrupt status.
-                        Thread.currentThread().interrupt();
-                    }
-                }
-            }).start();
-        }
-    }
-
-    @Override
-    public void onCreate() {
-        Log.e("BLE Service", "onCreate");
-        Tick();
-        /*HandlerThread thread = new HandlerThread("ServiceStartArguments",
-                Process.THREAD_PRIORITY_BACKGROUND);
-        thread.start();
-
-        // Get the HandlerThread's Looper and use it for our Handler
-        serviceHandler = new ServiceHandler(serviceLooper);
-        serviceLooper = thread.getLooper();*/
-
-        // Intent intent = new Intent(this, BluetoothService.class);
-        // startService(intent);
-    }
-
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        // If we get killed, after returning from here, restart
+        Log.i("BLE", "onStartCommand");
+        super.onStartCommand(intent, flags, startId);
         return START_STICKY;
-    }
-
-    public BluetoothService() {
-
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (bluetoothAdapter == null) {
-            // Device doesn't support Bluetooth
-        }
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        Log.e("BLE Service", "onBind");
-
-        if (!bluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            // startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-        }
-
-        // Tick();
-
-        return mBinder;
     }
 
     @Override
     public void onDestroy() {
-        Toast.makeText(this, "service done", Toast.LENGTH_SHORT).show();
+        super.onDestroy();
+        Log.i("BLE", "ondestroy!");
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
     }
 }
--- app/src/main/java/com/example/mishkatoy/MainActivity.java
+++ app/src/main/java/com/example/mishkatoy/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.mishkatoy;
 
+import android.Manifest;
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -18,31 +26,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener, ServiceConnection {
+        implements NavigationView.OnNavigationItemSelectedListener {
 
-    private BluetoothService s = null;
+    BluetoothService mService;
+    boolean mBound = false;
+    boolean bluetoothScanAccess = false;
+    ArrayList<BluetoothDevice> list = new ArrayList<BluetoothDevice>();
+    static final int MY_PERMISSION_REQUEST_CONSTANT = 1;
 
-    void launchBLEService() {
-        // Launch service
+    void bindBLE() {
+        Log.e("MAIN", "onBind");
         Intent intent = new Intent(this, BluetoothService.class);
-        bindService(intent, this, Context.BIND_AUTO_CREATE);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
     }
 
-    @Override
-    public void onServiceConnected(ComponentName name, IBinder binder) {
-        BluetoothService.BluetoothBinder b = (BluetoothService.BluetoothBinder) binder;
-        s = b.getService();
-        Toast.makeText(MainActivity.this, "Connected", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-    public void onServiceDisconnected(ComponentName name) {
-        s = null;
-    }
+    Context context;
+    ListView bleDeviceList;
+    final Context ctx = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,9 +64,13 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                launchBLEService();
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (mBound) {
+                    list.clear();
+                    list.addAll(mService.getDevices());
+                    bleDeviceList.setAdapter(new ArrayAdapter<>(ctx,
+                            android.R.layout.simple_list_item_1, list));
+                    mService.searchBLE();
+                }
             }
         });
 
@@ -70,32 +83,120 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        final Button button = findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (s != null) {
-                    s.vibrate();
-                    int counter = s.getCounter();
-                    Log.e("BLE Service", "Counter: " + Integer.toString(counter));
-                }
+        bleDeviceList = (ListView) findViewById(R.id.deviceList);
+        bleDeviceList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?>adapter, View v, int position, long id){
+                Object item = adapter.getItemAtPosition(position);
+                Log.e("onItemClick", item.toString() + " at " + position + ", id: " + id);
+                mService.connectBLE(list.get(position));
             }
         });
+
+        // Bind to LocalService
+        Intent intent = new Intent(this, BluetoothService.class);
+        startService(intent);
+        bindBLE();
+
+        context = getApplicationContext();
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(receiver, filter);
+
+        // Request permissions to scan bluetooth devices
+        String perm[] = {Manifest.permission.ACCESS_COARSE_LOCATION};
+        ActivityCompat.requestPermissions(this, perm, MY_PERMISSION_REQUEST_CONSTANT);
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (s == null) {
-            Intent intent= new Intent(this, BluetoothService.class);
-            bindService(intent, this, Context.BIND_AUTO_CREATE);
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSION_REQUEST_CONSTANT: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    bluetoothScanAccess = true;
+                } else {
+                    bluetoothScanAccess = false;
+                }
+            }
         }
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        unbindService(this);
+    protected void onDestroy() {
+        Log.e("MAIN", "onDestroy!");
+        super.onDestroy();
+        unbindService(connection);
+        unregisterReceiver(receiver);
     }
+
+    @Override
+    protected void onResume() {
+        Log.e("MAIN", "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.e("MAIN", "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.e("MAIN", "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.e("MAIN", "onStop");
+        super.onStop();
+    }
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private final BroadcastReceiver receiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            Log.e("BROADCAST", "onReceive");
+            String action = intent.getAction();
+            if (BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(action)) {
+                Log.e("BROADCAST", "ACTION_DISCOVERY_STARTED");
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                Log.e("BROADCAST", "ACTION_DISCOVERY_FINISHED");
+            } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+                list.add(device);
+                bleDeviceList.setAdapter(new ArrayAdapter<BluetoothDevice>(ctx,
+                        android.R.layout.simple_list_item_1, list));
+                Log.e("BROADCAST", device.getName());
+            }
+        }
+    };
+
+    private ServiceConnection connection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            Log.e("MAIN", "onServiceConnected!!!");
+            BluetoothService.LocalBinder binder = (BluetoothService.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+            if (mService.startBLE() != 0) {
+                Toast toast = Toast.makeText(context, "Cannot start bluetooth ;(", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            Log.e("MAIN", "onServiceDisconnected!!!");
+            mBound = false;
+        }
+    };
 
     @Override
     public void onBackPressed() {
